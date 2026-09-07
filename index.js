7ab807f835c9c1a85f16344f7d695a8bb9bbd165
Simpi Sharma <simpis825@gmail.com>
2026-09-07 19:39:40 +0530

    Learn:Context-Remember's the previous data

--- index.js
+++ index.js
@@ -3,15 +3,26 @@ import OpenAI from "openai";
 import dotenv from "dotenv";
 dotenv.config();
 
+const context =[
+  {
+    role:'system',
+    content:'keep answer short and simple'
+  }
+]
 const client = new OpenAI({
     apiKey: process.env.openai_key,
      baseURL: "https://api.groq.com/openai/v1" //this will send the request to groq api instead of openai api
 })
 async function aiAnswer(qsn){
+  context.push({role:'user',content:qsn})
     const response = await client.responses.create({
         model : "openai/gpt-oss-20b",
-        input:qsn
+        input:context
     })
+    context.push({role:'assistant',content:response.output_text})
+    //this si to remember what we have asked and what the ai has answered so that it can answer in context
+    console.log(context)
+    //but this increases the token bcs it remembers the whole conversation so it will be better to keep the context small and simple
      console.log(response.output_text);
 }
 //aiAnswer();
