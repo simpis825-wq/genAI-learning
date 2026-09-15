99b70e584d453a4e4f991e83433815e83e182cdf
Simpi Sharma <simpis825@gmail.com>
2026-09-15 15:51:50 +0530

    Learning:Using Gemini_Key

--- index.js
+++ index.js
@@ -1,48 +1,16 @@
-
-import OpenAI from "openai";
+import { GoogleGenAI } from "@google/genai";
 import dotenv from "dotenv";
-import express from "express";
-import multer from "multer";
-import path from "path";
-import { createReadStream, writeFileSync } from "fs";
-
-const app = express();
 dotenv.config();
-app.use(express.urlencoded({ extended: true })); // this is the middleware to help the post request
-
-const client = new OpenAI({
-    apiKey: process.env.openai_key,
-     baseURL: "https://api.groq.com/openai/v1" //this will send the request to groq api instead of openai api
-})
-
-//Route
-app.get("/", (req, res) => {
-    res.send(`<form action="/audio" method="post">
-      <input type="text" name="inputData"/> 
-      <br/>
-      <br/>
-      <button>Convert text in Audio</button>
-      
-      </form>`);
+const googleGenAI = new GoogleGenAI({
+  apiKey: process.env.gemini_key,
 });
 
-app.post("/audio",async (req,res)=>{
-  
-  await main(req,res);
-})
-async function main(req,res){
-  const response = await client.audio.speech.create({
-    model:"canopylabs/orpheus-v1-english",
-    input:req?.body?.inputData,
-    voice:"troy",
-    response_format: "wav",
-  });
-  const baseResponse = Buffer.from(await response.arrayBuffer());
-  writeFileSync("audio.mp3",baseResponse);
-  console.log(baseResponse);
-   res.send("Text Converted to Audio");
-  
+async function main(){
+  const response = await googleGenAI.models.generateContent({
+    model: "gemini-3.6-flash",
+    contents:"What is the capital of France?",
+  })
+  console.log(response.text);
 }
 
-//main();
-app.listen(3200);
+main();
